85beca4f036cda394878d0d3c60a3e2e9f962ba0
AlShammari <afnfun@yahoo.com>
2012-11-15 00:43:12 +0100

    update

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,11 @@
 <?php
-$hostname="localhost";
-$username="root";
-$password="root";
-$db="info2";
-//
-$connect =mysql_connect($hostname,$username,$password);
-mysql_select_db($db,$connect);
-//
 include('class.php');
 include('class.uFlex.php');
 include('Simple_Image.php');
 include('defines.php');
+include('databaseConfig.php');
+$connect =mysql_connect($hostname,$username,$password);
+mysql_select_db($db,$connect);
 //	
 $user = new uFlex();	       
 $reg= new info_registration;	
